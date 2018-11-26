363f492b23147483d009395938b3900ea531c05f
Michał Hordyjewicz <hordyjewiczmichal@gmail.com>
2018-11-26 15:33:35 +0100

    test written

---
+++ src/test/java/pl/coderslab/service/FactorsTest.java
@@ -0,0 +1,25 @@
+package pl.coderslab.service;
+
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class FactorsTest
+{
+    @Test
+    public void generatePrimeFactorsTest()
+    {
+        // given
+        int n = 99;
+        int[] expected = {1, 11, 33, 99};
+
+        // when
+        int[] result = Factors.generatePrimeFactors(n);
+
+        // then
+        assertThat("Wrong result", result, is(expected));
+    }
+
+}
